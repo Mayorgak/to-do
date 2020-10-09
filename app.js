80154edc9b2ae0fd6af09c259af21c784f7492e2
Kristen M <kristenmayorga@gmail.com>
2020-10-08 23:19:27 -0400

    added work page

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 let items = ["Buy Food", "Eat food"];
+let workItems = [];
 
 
 app.set('view engine', 'ejs');
@@ -22,16 +23,25 @@ app.get("/", function(req, res){
 
 let day = today.toLocaleDateString("en-us", options);
 
-    res.render('list', {kindOfDay: day, newListItems: items});
+    res.render('list', {listTitle: day, newListItems: items});
 });
 
-app.post("/", function (req,res) {
-    let item = req.body.newItem;
+app.post("/", function(req, res) {
 
+  let item = req.body.newItem;
+
+  if (req.body.list === "Work") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
     items.push(item);
-   
     res.redirect("/");
-      
+  }
+});
+
+
+app.get("/work" ,function( req,res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
 });
 
 
