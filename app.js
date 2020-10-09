a07e72c0017cc1e4b6c7cb3499c972fee34f136b
Kristen M <kristenmayorga@gmail.com>
2020-10-09 18:14:15 -0400

    fixexd delete button for today page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require ("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 
 const app = express();
@@ -85,7 +86,8 @@ app.post("/", function (req, res) {
 });
 
 app.get("/:customListName", function (req,res){
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
+  const listName = req.body.listName;
   
   List.findOne({name: customListName}, function(err,foundList){
     if(!err){
@@ -108,20 +110,20 @@ app.get("/:customListName", function (req,res){
   });
 });
 
-
-
 app.post("/delete",function(req, res){
   const checkedItemId = req.body.checkbox;
- 
+  // const listName = req.body.listName;
 
- Item.findByIdAndRemove(checkedItemId, function (err) {
+  Item.findByIdAndRemove(checkedItemId, function (err) {
    if (!err) {
      console.log("Susscessfully deleted checked item.");
      res.redirect("/");
    }
- });
  
-});
+   });
+  });
+
+
 
 app.get("/work" ,function( req,res) {
     res.render("list", {listTitle: "Work List", newListItems: workItems});
